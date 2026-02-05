b31e84aa6f3bd9bfa38361667ba92e79aa79e468
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-02-05 13:59:49 +0100

    stage 1

--- Pawns-Only Chess/task/src/chess/Main.kt
+++ Pawns-Only Chess/task/src/chess/Main.kt
@@ -1,5 +1,21 @@
 package chess
 
 fun main() {
-//    write your code here
+    println(" Pawns-Only Chess")
+
+    val files = listOf("a", "b", "c", "d", "e", "f", "g", "h")
+    val ranks = (8 downTo 1).toList()
+
+    ranks.forEach { rank ->
+        println("  +---+---+---+---+---+---+---+---+")
+        val content = when (rank) {
+            7 -> " B "
+            2 -> " W "
+            else -> "   "
+        }
+        println("$rank |$content|$content|$content|$content|$content|$content|$content|$content|")
+    }
+    println("  +---+---+---+---+---+---+---+---+")
+    println("    ${files.joinToString("   ")}")
+
 }
